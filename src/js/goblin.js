9edc13f9319cb01f64e1dfc4331971b53375d774
Ekaterina <kateshenyang@gmail.com>
2024-09-04 18:36:00 +0300

    lint fixed

--- src/js/goblin.js
+++ src/js/goblin.js
@@ -1,17 +1,16 @@
 export default class Goblin {
-    constructor () {
-        this.goblin = null;
+  constructor() {
+    this.goblin = null;
+  }
 
-    }
+  createGoblin() {
+    const goblin = document.createElement("div");
+    goblin.classList.add("goblin");
+    this.goblin = goblin;
+  }
 
-    createGoblin () {
-        const goblin = document.createElement('div')
-        goblin.classList.add('goblin')
-        this.goblin = goblin
-    }
-
-    getGoblin() {
-        this.createGoblin()
-        return this.goblin
-    }
+  getGoblin() {
+    this.createGoblin();
+    return this.goblin;
+  }
 }
